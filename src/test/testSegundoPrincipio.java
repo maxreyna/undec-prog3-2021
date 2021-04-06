2714c391b8afc569251fded6ea38294834aa390c
Max Reyna <maxla070212@gmail.com>
2021-04-06 14:33:22 -0300

    [TP1] Se crearon clases en paquetes

--- src/test/testSegundoPrincipio.java
+++ src/test/testSegundoPrincipio.java
@@ -1,4 +1,16 @@
 package test;
 
+import dominioPrimerPrincipio.Automovil;
+import dominioSegundoPrincipio.AutomovilDeCarrera;
+import org.junit.jupiter.api.Test;
+
 public class testSegundoPrincipio {
+
+    @Test
+    void Test01_acelerarAutomovil(){
+        Automovil a1 = new Automovil("Ford","Rojo");
+        a1.acelera();
+        Automovil a2 = new AutomovilDeCarrera("Renault","Rojo");
+        a2.acelera();
+    }
 }
